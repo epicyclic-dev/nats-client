79a45fd2e3ae2b3d214a71b01a12078423926fee
torque <torque@epicyclic.dev>
2023-08-15 00:19:51 -0700

    git subrepo clone (merge) --branch=v3.6.1 https://github.com/nats-io/nats.c.git deps/nats.c
    subrepo:
      subdir:   "deps/nats.c"
      merged:   "66cec7f"
    upstream:
      origin:   "https://github.com/nats-io/nats.c.git"
      branch:   "v3.6.1"
      commit:   "66cec7f"
    git-subrepo:
      version:  "0.4.6"
      commit:   "b8b46501e"

---
+++ deps/nats.c/doc/html/navtreedata.js
@@ -0,0 +1,63 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "NATS C Client with JetStream and Streaming support", "index.html", [
+    [ "NATS C client.", "index.html", "index" ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group__libevent_functions.html#ga30c00f21a5251ee9fde09c1f2b429cd2",
+"group__types_group.html#gga410f8fb053efa02b2ca1de0c02fa4b6dae874de4a787cfbec4c6d0eb1a3f05ebd",
+"structjs_consumer_config.html#a2f0243899885dc97f304f48ca52b5356",
+"structkv_keys_list.html#aa5cb40820a1a927375215e0a94ecfc18"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ deps/nats.c/doc/html/navtreeindex4.js
@@ -0,0 +1,15 @@
+var NAVTREEINDEX4 =
+{
+"structkv_keys_list.html#aa5cb40820a1a927375215e0a94ecfc18":[2,0,40,0],
+"structkv_purge_options.html":[2,0,38],
+"structkv_purge_options.html#a245c2def0f3aabe617219772cf7680a8":[2,0,38,0],
+"structkv_purge_options.html#a47fed1e95073666cbe441aba7c149da6":[2,0,38,1],
+"structkv_watch_options.html":[2,0,37],
+"structkv_watch_options.html#a0535edd21b26affc0057575bafc21a07":[2,0,37,0],
+"structkv_watch_options.html#a08274f08ae96e14d9ded326e4e665649":[2,0,37,1],
+"structkv_watch_options.html#a56183d56585697d058c7eeb20bdddb66":[2,0,37,3],
+"structkv_watch_options.html#ac9094bce4f3c6513431ba32fb49e5605":[2,0,37,2],
+"structnats_msg_list.html":[2,0,0],
+"structnats_msg_list.html#a5c48bf6b9e7f1d889d7ad31694911750":[2,0,0,1],
+"structnats_msg_list.html#aa123668879f1c9c11c9be7bd81a205c5":[2,0,0,0]
+};
